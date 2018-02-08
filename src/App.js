f299afbcc91207a2e830cb696946056392b7215b
yuting.song <yutingsongzenjoy@126.com>
2018-02-08 20:21:15 +0800

    basic component and scss

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 // import logo from './Y.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="poem-content">
-          故园东望路漫漫，双袖龙钟泪不干。<br></br>
-          马上相逢无纸笔，凭君传语报平安。
+          <div className="poem-text">
+            故园东望路漫漫，双袖龙钟泪不干。<br></br>
+            马上相逢无纸笔，凭君传语报平安。
+          </div>
         </div>
         <span className="poem-author">- 岑参 -</span>
-        <div className="footer">
+        <div className="poem-ctrl">
           <button className="btn detail">诗意</button>
           <button className="btn random">邂逅</button>
         </div>
+        <footer className="footer">
+          <a href="https://github.com/Unknow-Y/poetry-encounter">
+             Code by Soyaine with ❤ host on GitHub
+          </a>
+        </footer>
       </div>
     );
   }
